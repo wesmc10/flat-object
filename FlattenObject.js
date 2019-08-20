3875e0e378e14194dbf98bf2a18811a92e8fe282
Wes McElroy <mcelroywes@gmail.com>
2019-08-19 19:24:05 -0700

    Modified comments

--- FlattenObject.js
+++ FlattenObject.js
@@ -1,4 +1,4 @@
-function flattenObject(obj) {
+function flatObject(obj) {
 	const returnObj = {};
     
 	for (const key in obj) {
@@ -7,12 +7,12 @@ function flattenObject(obj) {
             // if the value is an object, make a recursive call with that value as the parameter.
             // we'll do this until we get to the base case, which is that the value of
             // whatever key we're on is now a string
-            const flatObject = flattenObject(obj[key]);
-            // loop through the flatObject that's been created with each recursive call and add
+            const newObject = flatObject(obj[key]);
+            // loop through the newObject that's been created with each recursive call and add
             // a key to the return object. the key should contain the correct key given the current position in
-            // the call stack, concatenated with a dot and the value of the current key in the flatObject.
-            // it should also set its value to be the value of the current key in the flatObject
-            for (const nestedKey in flatObject) returnObj[`${key}.${nestedKey}`] = flatObject[nestedKey];
+            // the call stack, concatenated with a dot and the value of the current key in the newObject.
+            // it should also set its value to be the value of the current key in the newObject
+            for (const nestedKey in newObject) returnObj[`${key}.${nestedKey}`] = newObject[nestedKey];
             // if the value of the data type is 'string', add it to the return object
 		} else returnObj[key] = obj[key];
     }
