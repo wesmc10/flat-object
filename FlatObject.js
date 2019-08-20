72384750c3241b76a8d7cc878667770ecf4c3c45
Wes McElroy <mcelroywes@gmail.com>
2019-08-19 20:01:58 -0700

    Modified spacing

--- FlatObject.js
+++ FlatObject.js
@@ -1,12 +1,12 @@
 function flatObject(obj) {
-	const returnObj = {};
+    const returnObj = {};
     
-	for (const key in obj) {
-		if (typeof obj[key] === 'string') {
-      		returnObj[key] = obj[key];
-    	} else {
-			/* if the value is an object (or an array, since the data type of an array in JS is 'object'),
-			make a recursive call with that value as the parameter.
+    for (const key in obj) {
+        if (typeof obj[key] === 'string') {
+            returnObj[key] = obj[key];
+        } else {
+            /* if the value is an object (or an array, since the data type of an array in JS is 'object'),
+            make a recursive call with that value as the parameter.
             we'll do this until we get to the base case, which is that the value of
             whatever key we're on is now a string */
             const newObject = flatObject(obj[key]);
@@ -15,8 +15,8 @@ function flatObject(obj) {
             the call stack, concatenated with a dot and the value of the current key in the newObject.
             it should also set its value to be the value of the current key in the newObject */
             for (const nestedKey in newObject) returnObj[`${key}.${nestedKey}`] = newObject[nestedKey];
-		}
-  	}
+        }
+    }
     return returnObj;
 };
 
